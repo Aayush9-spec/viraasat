4b6e250263d6781b7dadd514f4d2a6dae26fbe53
Aayush9-spec <2k24.cs1a.2414142@gmail.com>
2026-05-07 09:06:27 +0530

    refactor(dashboard): remove redundant logo from sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -26,7 +26,7 @@ export default function DashboardLayout({
       <div className="hidden border-r bg-card md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <ViraasatLogo />
+            {/* Logo removed as it's in global navbar */}
           </div>
           <div className="flex-1">
             <DashboardNav />
